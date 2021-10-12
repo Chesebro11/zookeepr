155f7e44f17fc11fcc2e24c01e0dc660496e5951
unknown <chesebrojared@gmail.com>
2021-10-11 22:00:10 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { animals } = require('./data/animals');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express ();
 
 function filterByQuery(query, animalsArray) {
@@ -51,6 +51,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`APO server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`APO server now on port ${PORT}`);
 });
