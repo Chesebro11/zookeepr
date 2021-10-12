91408167fc9643d940425d5799a65cfb3b23e8b9
unknown <chesebrojared@gmail.com>
2021-10-12 00:26:18 -0700

    can now post

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express ();
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -64,6 +68,12 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
   });
+
+  app.post('/api/animals', (req, res) => {
+      // req.body is where our incoming content will be
+      console.log(req.body)
+      res.json(req.body);
+  })
 
 app.listen(PORT, () => {
     console.log(`APO server now on port ${PORT}`);
